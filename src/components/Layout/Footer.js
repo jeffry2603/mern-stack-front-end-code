b687e12ec5af2334fda2e2402c6a8bf37b17eeca
Jeffry Allan <108166134+jeffry2603@users.noreply.github.com>
2023-09-21 20:51:52 +0530

    mern front end code

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h1 className="text-center">All Rights Reserved &copy; Jeffry Allan</h1>
+      <p className="text-center mt-3">
+        <Link to="/about">About Us</Link>|<Link to="/contact">Contact Us</Link>|
+        <Link to="/policy">Privacy & Policy</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
